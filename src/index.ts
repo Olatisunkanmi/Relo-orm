22b972309df286719b31ff2f412f2dcdf130c27e
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-10-06 15:24:01 +0100

    initial commit but how did I get here?

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+"use-strict";
+
+/**
+ * An Relo Module that contains the Relo entry point.
+ *
+ * @module Relo
+ */
+
+/** Exports the xl sequelize point. */
+const { Relo } = require("./reno");
+
+
+/** Exports the xl sequelize point. */
+module.exports = Relo;
