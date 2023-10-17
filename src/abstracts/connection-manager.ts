c0500c16234efab248a1f27b332d50fff6f788a5
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-10-17 05:04:32 +0100

    feat: app abstracts

---
+++ src/abstracts/connection-manager.ts
@@ -0,0 +1,19 @@
+import { ConnectionOptions, Relo } from "../relo";
+
+export interface Connection {
+  uuid?: string;
+}
+
+export abstract class ConnectionManager {
+  //connect instance of Relo to database
+  protected async _connect(config: ConnectionOptions): Promise<Connection> {
+    console.log("Connecting to database...");
+    throw new Error("Method not implemented.");
+  }
+
+  //disconnect instance of Relo from database
+  protected async _disconnect(): Promise<void> {
+    console.log("Disconnecting from database...");
+    throw new Error("Method not implemented.");
+  }
+}
