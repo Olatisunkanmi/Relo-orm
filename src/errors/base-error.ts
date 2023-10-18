4e56200e1dba428f6ba1d4eb16523d8bb254f3c6
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-10-18 05:04:45 +0100

    feat: error handlers

---
+++ src/errors/base-error.ts
@@ -0,0 +1,27 @@
+/**
+ * The Base Error for all Relo errors.
+ *
+ * All Relo errors inherit from this class.
+ * @class BaseError
+ */
+export class BaseError extends Error {
+  declare cause?: any;
+  declare options?: object;
+
+  /**
+   * The Base Error constructor.
+   * Instantiates the Base Error class with a message.
+   *
+   * @param {string} message - The message of the error.
+   * @param {any} [cause] - The cause of the error.
+   * @param {object} [options] - The options of the error.
+   * @memberof BaseError
+   */
+    constructor(message: string, cause?: any, options?: object) {
+    super(message);
+    this.cause = cause;
+    this.options = options;
+    
+    }
+
+}
