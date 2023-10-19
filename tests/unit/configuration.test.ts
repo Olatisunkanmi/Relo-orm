82316e3c5d9b961ce0067f89a5dbcb60b2bca0c4
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-10-19 05:04:04 +0100

    test: configuartion test -incomplete

---
+++ tests/unit/configuration.test.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { Relo, getTestDialect } from "../support";
+
+const dialect = getTestDialect();
+describe("Relo Constructor", () => {
+  it("throws when no dialect is supplied", () => {
+    expect(() => {
+      new Relo("localhost, test", "test-password");
+    }).to.throw(Error);
+  });
+
+  it("throws when dialect is not an object", () => {
+    expect(() => {
+      new Relo("localhost, test", "test-password", "test");
+    }).to.throw(Error);
+  });
+
+  it("works when dialect is supplied", () => {
+    expect(() => {
+      new Relo("localhost", "test", "test", {
+        dialect,
+      });
+    }).not.to.throw(Error);
+  });
+});
+
